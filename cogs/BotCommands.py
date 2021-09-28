f499879e169733ea0c9de3f251c4e0754f1b9cea
zinnia <klossabby@gmail.com>
2021-09-27 23:01:18 -0500

    embeds and embeds testing

--- cogs/BotCommands.py
+++ cogs/BotCommands.py
@@ -99,15 +99,16 @@ def checkString(string):
 
     return mount
 
-#outputs a string that's 15 characters long
-def checkName(string):
+#outputs a string that's 'length' characters long
+#i s2g don't make length 2 or less characters
+def checkName(string, length=15):
     output = ""
-    if (len(string) <= 15):
+    if (len(string) <= length):
         output = string
-        for i in range(15 - len(string)):
+        for i in range(length - len(string)):
             output += " "
     else:
-        output = string[:12] + "..."
+        output = string[:length-3] + "..."
 
     return output
 
@@ -125,7 +126,7 @@ class BotCommands(commands.Cog, description="most of the commands"):
             #if there isn't a row for the user, make one
             cur.execute("select exists(select * from Users where UserID=?)", (int(ctx.author.id),))
             if (not cur.fetchone()[0]):
-                cur.execute("insert into Users values (?, ?, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)", (int(ctx.author.id), int(ctx.guild.id)))
+                cur.execute("insert into Users values (?, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)", (int(ctx.author.id), ))
             
             #defaults for now
             mount = ""
@@ -184,40 +185,40 @@ class BotCommands(commands.Cog, description="most of the commands"):
         cur = con.cursor()
 
         for row in con.execute("select * from Users where UserID=?", (ctx.author.id, )):
-            ARRB = str(row[2])
-            ARRM = str(row[3])
-            ARRE = str(row[4])
-            ARRG = str(row[5])
-            ARRX = str(row[6])
-            ARRA = str(row[7])
-            ARRN = str(row[8])
+            ARRB = str(row[1])
+            ARRM = str(row[2])
+            ARRE = str(row[3])
+            ARRG = str(row[4])
+            ARRX = str(row[5])
+            ARRA = str(row[6])
+            ARRN = str(row[7])
 
-            HWDe = str(row[9])
-            HWSo = str(row[10])
-            HWDa = str(row[11])
-            HWWa = str(row[12])
-            HWRn = str(row[13])
-            HWRs = str(row[14])
-            HWWh = str(row[15])
+            HWDe = str(row[8])
+            HWSo = str(row[9])
+            HWDa = str(row[10])
+            HWWa = str(row[11])
+            HWRn = str(row[12])
+            HWRs = str(row[13])
+            HWWh = str(row[14])
 
-            SBHa = str(row[16])
-            SBEu = str(row[17])
-            SBLu = str(row[18])
-            SBAu = str(row[19])
-            SBLe = str(row[20])
-            SBRe = str(row[21])
-            SBBl = str(row[22])
+            SBHa = str(row[15])
+            SBEu = str(row[16])
+            SBLu = str(row[17])
+            SBAu = str(row[18])
+            SBLe = str(row[19])
+            SBRe = str(row[20])
+            SBBl = str(row[21])
 
-            ShBD = str(row[23])
-            ShBE = str(row[24])
-            ShBL = str(row[25])
-            ShBR = str(row[26])
-            ShBS = str(row[27])
-            ShBI = str(row[28])
-            ShBF = str(row[29])
+            ShBD = str(row[22])
+            ShBE = str(row[23])
+            ShBL = str(row[24])
+            ShBR = str(row[25])
+            ShBS = str(row[26])
+            ShBI = str(row[27])
+            ShBF = str(row[28])
 
-        string = '''```        ARR       |        HW        |        SB         |        ShB        
-__________|_______|__________|_______|___________|_______|__________|_______
+        string = '''```         ARR       |         HW        |         SB        |         ShB        
+___________|_______|___________|_______|___________|_______|___________|_______
 Mount      | Have? |   Mount   | Have? |   Mount   | Have? |   Mount   | Have?
 ___________|_______|___________|_______|___________|_______|___________|_______
 Boreas     | '''+ARRB+'''     | Demonic   | '''+HWDe+'''     | Hallowed  | '''+SBHa+'''     | Diamond   | '''+ShBD+'''
@@ -233,6 +234,7 @@ note: 0 is no, 1 is yes```'''
         con.close()
         await ctx.reply(string, mention_author=False)
 
+    '''
     @commands.command(name="hasMounts", help="checks who, in the server, has specific mount or an expac's mounts", usage="<mount name | expac>")
     async def has_mounts(self, ctx, *args):
         reply = "None"
@@ -307,18 +309,104 @@ note: 0 is no, 1 is yes```'''
                 reply += "```"
             
             else:
-                reply += "``` Name           | " + mount + "\n"
+                reply += "```Name           | " + mount + "\n"
                 async for member in ctx.guild.fetch_members(limit=150):
                     for row in cur.execute("select " + mount + " from Users where userID=?", (int(member.id), )):
-                        reply += " " + str(member.display_name)
-                        for i in range(15 - len(member.display_name)):
-                            reply += " "
+                        reply += checkName(str(member.display_name))
                         reply +=  "| " + str(row[0]) + "\n"
                 reply += "```"
 
         
-        await ctx.reply(reply, mention_author=False)
+        await ctx.reply(reply, mention_author=False)'''
 
+    @commands.command(name='hasMounts', help='help')
+    async def has_mounts(self, ctx, *args):
+        if (len(args) == 1):
+            embed=discord.Embed(title="Server Mounts", color=ctx.author.color)
+            embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar_url)
+            embed.set_footer(text="note: 0 is no, 1 is yes")
+            length = 7
+
+            con = sqlite3.connect('files/EXMountTracker.db')
+            cur = con.cursor()
+
+            reply = ""
+            mount = checkString(args[0])
+
+            if (mount == ""):
+                await ctx.reply("usage: <mount|expac>", mention_author=False)
+                return
+
+            elif (mount == "ARR"):
+                reply += "```Name" + (" " * (length-4))
+                for item in ARRList:
+                    reply += "|" + item
+                reply += "\n"
+                async for member in ctx.guild.fetch_members(limit=150):
+                    cur.execute("select exists(select UserID from Users where userID=?)", (member.id, ))
+                    if (cur.fetchone()[0]):
+                        reply += checkName(member.display_name, length)
+                        for item in ARRList:
+                            for row in cur.execute("select " + item + " from Users where userID=?", (int(member.id), )):
+                                reply +=  "| " + str(row[0]) + "  "
+                        reply += '\n'
+
+            elif (mount == "HW"):
+                reply += "```Name           "
+                for item in HWList:
+                    reply += "|" + item
+                reply += "\n"
+                async for member in ctx.guild.fetch_members(limit=150):
+                    cur.execute("select exists(select UserID from Users where userID=?)", (member.id, ))
+                    if (cur.fetchone()[0]):
+                        reply += checkName(member.display_name, length)
+                        for item in HWList:
+                            for row in cur.execute("select " + item + " from Users where userID=?", (int(member.id), )):
+                                reply +=  "| " + str(row[0]) + "  "
+                        reply += '\n'
+
+            elif (mount == "SB"):
+                reply += "```Name           "
+                for item in SBList:
+                    reply += "|" + item
+                reply += "\n"
+                async for member in ctx.guild.fetch_members(limit=150):
+                    cur.execute("select exists(select UserID from Users where userID=?)", (member.id, ))
+                    if (cur.fetchone()[0]):
+                        reply += checkName(member.display_name, length)
+                        for item in SBList:
+                            for row in cur.execute("select " + item + " from Users where userID=?", (int(member.id), )):
+                                reply +=  "| " + str(row[0]) + "  "
+                        reply += '\n'
+            
+            elif (mount == "ShB"):
+                reply += "```Name           "
+                for item in ShBList:
+                    reply += "|" + item
+                reply += "\n"
+                async for member in ctx.guild.fetch_members(limit=150):
+                    cur.execute("select exists(select UserID from Users where userID=?)", (member.id, ))
+                    if (cur.fetchone()[0]):
+                        reply += checkName(member.display_name, length)
+                        for item in ShBList:
+                            for row in cur.execute("select " + item + " from Users where userID=?", (int(member.id), )):
+                                reply +=  "| " + str(row[0]) + "  "
+                        reply += '\n'
+            
+            else:
+                reply += "```Name           | " + mount + "\n"
+                async for member in ctx.guild.fetch_members(limit=150):
+                    for row in cur.execute("select " + mount + " from Users where userID=?", (int(member.id), )):
+                        reply += checkName(str(member.display_name))
+                        #for i in range(15 - len(member.display_name)):
+                        #    reply += " "
+                        reply +=  "| " + str(row[0]) + "\n"
+                
+            reply += "```"
+            embed.add_field(name=mount, value=reply, inline=False)
+            await ctx.reply(embed=embed, mention_author=False)
+        else:
+            await ctx.reply("usage: <mount|expac>", mention_author=False)
     
 
 def setup(bot):
--- cogs/Info.py
+++ cogs/Info.py
@@ -1,5 +1,6 @@
 from discord.ext.commands.core import has_permissions
 from discord.ext import commands
+import discord
 import sqlite3
 
 class Info(commands.Cog, description="basic info"):
@@ -8,17 +9,17 @@ class Info(commands.Cog, description="basic info"):
 
     @commands.command(name='table', help='shows how we represent the mounts by name')
     async def table(self, ctx):
-        string = '''```        ARR       |       HW        |       SB         |        ShB        
-___________|______|__________|______|___________|______|__________|_______
-   Word    |  DB  |   Word   |  DB  |   Word    |  DB  |   Word   |  DB   
-___________|______|__________|______|___________|______|__________|_______
- Boreas    | ARRB | Demonic  | HWDe | Hallowed  | SBHa | Diamond  | ShBD
- Markab    | ARRM | Sophic   | HWDo | Euphonius | SBEu | Emerald  | ShBE
- Enbarr    | ARRE | Dark     | HWDa | Lunar     | SBLu | Light    | ShBL
- Gullfaxi  | ARRG | Warring  | HWWa | Auspicious| SBAu | Ruby     | ShBR
- Xanthos   | ARRX | Round    | HWRn | Legendary | SBLe | Shadow   | ShBS
- Aithon    | ARRA | Rose     | HWRo | Reveling  | SBRe | Innocent | ShBI
- Nightmare | ARRN | White    | HWWh | Blissful  | SBBl | Fae      | ShBF
+        string = '''```      ARR     |     HW     |      SB       |     ShB     
+_________|____|_______|____|__________|____|________|____
+  Word   | DB |  Word | DB |  Word    | DB |  Word  | DB 
+_________|____|_______|____|__________|____|________|____
+Boreas   |ARRB|Demonic|HWDe|Hallowed  |SBHa|Diamond |ShBD
+Markab   |ARRM|Sophic |HWDo|Euphonius |SBEu|Emerald |ShBE
+Enbarr   |ARRE|Dark   |HWDa|Lunar     |SBLu|Light   |ShBL
+Gullfaxi |ARRG|Warring|HWWa|Auspicious|SBAu|Ruby    |ShBR
+Xanthos  |ARRX|Round  |HWRn|Legendary |SBLe|Shadow  |ShBS
+Aithon   |ARRA|Rose   |HWRo|Reveling  |SBRe|Innocent|ShBI
+Nightmare|ARRN|White  |HWWh|Blissful  |SBBl|Fae     |ShBF
 
 note: not case sensitive```'''
         await ctx.reply(string, mention_author=False)
@@ -39,6 +40,62 @@ note: not case sensitive```'''
     @commands.command(name='invite', help="invite me to another server")
     async def invite(self, ctx):
         await ctx.reply("Here's the link:\nhttps://discord.com/oauth2/authorize?client_id=891445253092036680&permissions=264192&scope=bot%20applications.commands", mention_author=False)
+
+    @commands.command(name='embedPages', help='proto-help')
+    async def embedPages(self, ctx):
+        page1 = discord.Embed (
+        title = 'Page 1/3',
+        description = 'Description',
+        colour = discord.Colour.orange()
+        )
+        page2 = discord.Embed (
+            title = 'Page 2/3',
+            description = 'Description',
+            colour = discord.Colour.orange()
+        )
+        page3 = discord.Embed (
+            title = 'Page 3/3',
+            description = 'Description',
+            colour = discord.Colour.orange()
+        )
+        
+        pages = [page1, page2, page3]
+
+        message = await ctx.send(embed = page1)
+        await message.add_reaction('⏮')
+        await message.add_reaction('◀')
+        await message.add_reaction('▶')
+        await message.add_reaction('⏭')
+
+        def check(reaction, user):
+            return user == ctx.author
+
+        i = 0
+        reaction = None
+
+        while True:
+            if str(reaction) == '⏮':
+                i = 0
+                await message.edit(embed = pages[i])
+            elif str(reaction) == '◀':
+                if i > 0:
+                    i -= 1
+                    await message.edit(embed = pages[i])
+            elif str(reaction) == '▶':
+                if i < 2:
+                    i += 1
+                    await message.edit(embed = pages[i])
+            elif str(reaction) == '⏭':
+                i = 2
+                await message.edit(embed = pages[i])
+            
+            try:
+                reaction, user = await self.wait_for('reaction_add', timeout = 30.0, check = check)
+                await message.remove_reaction(reaction, user)
+            except:
+                break
+
+        await message.clear_reactions()
 
 def setup(bot):
     bot.add_cog(Info(bot))
